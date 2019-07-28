3316daa6e4e31e059a457decd75b8c871ba2309d
aameen951 <aameen951@gmail.com>
2019-07-28 10:45:35 +0300

    Initial commit.

---
+++ index.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+
+function realpath(path, options)
+{
+  return new Promise((resolve, reject)=>{
+    fs.realpath(path, options, (err, resolvedPath) => {
+      if(err)reject(err);
+      else resolve(resolvedPath);
+    });
+  });
+}
+function writeFile(file, data, options)
+{
+  return new Promise((resolve, reject)=>{
+    fs.writeFile(file, data, options, (err) => {
+      if(err)reject(err);
+      else resolve();
+    });
+  });
+}
+function readFile(path, options)
+{
+  return new Promise((resolve, reject)=>{
+    fs.readFile(path, options, (err, data)=>{
+      if(err)reject(err);
+      else resolve(data);
+    });
+  });
+}
+function mkdir(path, options)
+{
+  return new Promise((resolve, reject)=>{
+    fs.mkdir(path, options, (err) => {
+      if(err)reject(err);
+      else resolve();
+    });
+  });
+}
+async function ensureDirExists(path, options)
+{
+  try
+  {
+    await mkdir(path, options);
+  }
+  catch(e)
+  {
+    if(e.code !== 'EEXIST') 
+      throw e;
+  }
+}
+module.exports = {
+  realpath,
+  readFile,
+  writeFile,
+  mkdir,
+  ensureDirExists,
+};
